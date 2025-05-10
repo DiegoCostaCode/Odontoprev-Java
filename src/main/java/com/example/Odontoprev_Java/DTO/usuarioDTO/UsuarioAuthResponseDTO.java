cf82bd3caddb109a6aa9f04169f0960b3d554a87
Diego Costa <diegocosta.contato2021@gmail.com>
2025-05-10 20:55:22 -0300

    [chore] - Voltando a nomenclaturas de atributos do DTO de UsuarioAuthResponse

--- src/main/java/com/example/Odontoprev_Java/DTO/usuarioDTO/UsuarioAuthResponseDTO.java
+++ src/main/java/com/example/Odontoprev_Java/DTO/usuarioDTO/UsuarioAuthResponseDTO.java
@@ -2,7 +2,7 @@ package com.example.Odontoprev_Java.DTO.usuarioDTO;
 
 public record UsuarioAuthResponseDTO
         (
-                Long id,
-                String token
+                Long userId,
+                String role
         ) {
 }
